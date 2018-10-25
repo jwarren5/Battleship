8b4d3685a953d5733721dfa3525d5f7bc303bd61
jwarren5 <44457026+jwarren5@users.noreply.github.com>
2018-10-25 17:24:02 -0500

    Update util.h

--- util.h
+++ util.h
@@ -1,9 +1,9 @@
 /**
-	CS 231 Project 2
+	CS 231 Project 3
 	util.h
 	Purpose: Header for several useful "utility functions" for the "Sea Battle!" game.
 	
-	@author Paul Zachos
+	@author Josh Warren
 	@version 1.0
 */
 
